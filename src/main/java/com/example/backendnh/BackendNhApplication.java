d20a06627c543b03bf68c7cc0bf90766e5c667ff
Truth360 <tzh19850355091@163.com>
2023-01-04 11:09:46 +0800

    test jenkins

--- src/main/java/com/example/backendnh/BackendNhApplication.java
+++ src/main/java/com/example/backendnh/BackendNhApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackendNhApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BackendNhApplication.class, args);
     }
